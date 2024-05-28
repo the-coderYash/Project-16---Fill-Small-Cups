931be7ab753c4aaec2886498bab0b857bc702f8f
Yash Raj <yashraj00112@gmail.com>
2024-05-28 20:44:54 +0530

    //Project Finished + //Added new styling to css

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-const counters = document.querySelectorAll('.counter');
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 
-counters.forEach((counter) => {
-  counter.innerText = '0';
+updateBigCup();
 
-  const updateCounter = () => {
-    const target = +counter.getAttribute('data-target');
-    const c = +counter.innerText;
-
-    const increment = target / 200;
-
-    if (c < target) {
-      counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 1);
-    } else {
-      counter.innerText = target;
-    }
-  };
-
-  updateCounter();
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
 });
+
+function highlightCups(idx) {
+  if (idx === 7 && smallCups[idx].classList.contains('full')) idx--;
+  else if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
